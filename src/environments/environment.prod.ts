f067fa0b34fa68f69d414839601509e901ffae87
Alioune DIOUF <adiouf@hn-services.center>
2021-04-30 10:15:36 +0200

    changement env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  url: 'http://localhost:8070/tb-api/api/',
-  base_url : 'http://localhost:8070/FRONTTableauDeBord/'
+  //url: 'http://RsExplRec.appli:8080/tb-api/api/',
+  //base_url : 'http://RsExplRec.appli:8080/FRONTTableauDeBord/'
+  url: 'http://localhost:8080/tb-api/api/',
+  base_url : 'http://localhost:8080/FRONTTableauDeBord/'
+
 };
--- src/environments/environment.recette.ts
+++ src/environments/environment.recette.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    url: 'http://RsExplRec.appli:<tomcat port>/tb-api/api/',
-    base_url : 'http://RsExplRec.appli:<tomcat port>/FRONTTableauDeBord/'
+    url: 'http://RsExplRec.appli:8080/tb-api/api/',
+    base_url : 'http://RsExplRec.appli:8080/FRONTTableauDeBord/'
   };
   
